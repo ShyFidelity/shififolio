16c7d95b3ee5d7fd697dff4aa3c557c22aeefa54
jkworks <info@jilliankayworks.com>
2022-02-05 17:08:08 -0700

    npm packages installed

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
-import Cactus from '../images/cactus2.png'
+import Planet from '../images/saturn.png'
 import AboutData from '../subcomponents/AboutData'
+
+import { motion } from 'framer-motion'
+import { lazy, Suspense } from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
 const boxStyle = {
     maxHeight: '500px',
     width: '100vw', 
@@ -8,6 +12,24 @@ const boxStyle = {
    
 
 }
+
+const Saturn = styled(motion.div)`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  animation: ${float} 4s ease infinite;
+width:20vw;
+  img{
+    width:100%;
+    height:auto;
+  }
+`
+
+const float = keyframes`
+0% { transform: translateY(-10px)         }
+    50% { transform: translateY(15px) translateX(15px)        }
+    100% { transform: translateY(-10px)         }
+`
 
 const About = () =>{
     return (
