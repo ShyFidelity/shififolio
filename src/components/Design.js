27d874106d7ba2f13edca7b7aa967dfab91ec340
jkworks <info@jilliankayworks.com>
2024-05-21 15:15:14 -0600

    commit

--- src/components/Design.js
+++ src/components/Design.js
@@ -2,6 +2,7 @@ import "../styles/portCard.css";
 import LV from "../images/LVSHAPES.svg";
 
 
+
 const boxStyle = {
   height: "500px",
   width: "100vw",
@@ -19,6 +20,16 @@ export default function Design() {
     <div className={boxStyle} designWork>
 
       <img width="1000px"src={LV} alt="Lv logos says Design Work" />
+      <ul>
+      
+      <li><a href="https://www.boltstack.com"> I designed and built this marketing Website's front end </a></li>
+      <li><a href="https://boltstack-dev.softwarebbd.com/rrframework/html/web/index.html?screenId=6632d4cf8f082a5fd00ac2e8&appId=6632d4b18f082a5fd00ac2e6&preview=1&rr_dev=true&"> Created data sets and front end fucntionality for this Desktop survey </a></li>
+      <li><a href="https://xd.adobe.com/view/27b4cbc7-d54b-442f-89c0-ca7aef44e648-72e7/?fullscreen"> Log in screens </a>
+</li>
+
+
+      </ul>
+   
     
       </div>
     
